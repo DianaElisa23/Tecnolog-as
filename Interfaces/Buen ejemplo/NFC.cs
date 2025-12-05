847a56a50a1a2051267260a83280d9b18b9b9558
Diana🍓 <zS23017370@estudiantes.uv.mx>
2025-12-04 23:03:54 -0600

    Delete Interfaces directory

--- Interfaces/Buen ejemplo/NFC.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace buenEjemplo
-{
-    public interface pagarConNFC
-    {
-        void Pagar(string numeroTarjeta, double monto);
-    }
-}
--- Interfaces/Mal ejemplo/ZTE.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace malEjemplo
-{
-    public class ZTE : Telefono
-    {
-        
-    } 
-}
